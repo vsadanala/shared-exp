05785f6fc746fd034e372d1b02fb164be50d83c1
learn-devops-fast <41976996+learn-devops-fast@users.noreply.github.com>
2020-12-06 18:28:32 -0500

    Create uatInput.groovy

---
+++ com/mcnz/jenkins/uatInput.groovy
@@ -0,0 +1,22 @@
+package com.mcnz.jenkins
+
+class uatInput {
+
+  def buildIsUatApproved() {
+    flag = false
+    file = new File("C:/_tools/approved.txt")
+    
+    if (file.exists()){
+      println "Printing Approval File Contents"
+      file.eachLine{line-> println line}
+    	flag = true
+    }
+    else {
+      println "Approval file does not exist."
+    }
+    
+    return flag;
+    
+  }
+
+}
