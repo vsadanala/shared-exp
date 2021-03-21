8aa791dd3b7fc42a972fea7f93925e93b1421cf9
vsadanala <38070798+vsadanala@users.noreply.github.com>
2021-03-21 23:43:19 +0530

    Update uatInput.groovy

--- src/com/mcnz/uatInput.groovy
+++ src/com/mcnz/uatInput.groovy
@@ -3,7 +3,7 @@ package com.mcnz
 public class uatInput {
 
   def buildIsUatApproved() {
-    def file = new File("/tmp/release.version")
+    def file = new File("/tmp/release.version1")
     
     if (file.exists()){
       return true;
